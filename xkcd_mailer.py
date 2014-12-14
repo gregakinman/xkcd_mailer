cf7a2a1294289bdf62a17749669c5573e025f018
Greg Kinman <gregakinman@gmail.com>
2014-12-14 02:28:49 -0600

    Cron job up and running.

--- xkcd_mailer.py
+++ xkcd_mailer.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 """
-A fun little script, designed to run in a cron job on Mondays, Wednesdays, and Fridays, to get xkcd automatically
+A fun little program, designed to run in a cron job on Mondays, Wednesdays, and Fridays, to get xkcd automatically
 delivered to your inbox.
 
-According to xkcd.com, xkcd updates without fail every Monday, Wednesday, and Friday, so, theoretically, running a cron job each of those days will get the latest comic. Still need to find out at what time it gets updated, though. Exact timing is a work in progress. I reckon checking for an update is another possible workaround, but we'll try this method first.
+According to xkcd.com, xkcd updates without fail every Monday, Wednesday, and Friday, so, theoretically, running a cron
+job each of those days will get the latest comic. Still need to find out at what time it gets updated, though. Exact
+timing is a work in progress. I reckon checking for an update is another possible workaround, but we'll try this method
+first.
 
-There's probably already a solution out there to do this, and/or an easier way to do this. But I don't really care,
-because this was fun to do.
+There are definitely easier & non-programmatic ways to do this (e.g., ITTT), but this was more of a fun exercise for
+learning cron jobs and helping myself to stop forgetting to read xkcd. Also, this has the added advantage of easily
+adding new emails to the list, so it's a mailing list, of sorts.
 """
 
 # Standard email, regex, and image scraping libraries.
@@ -25,87 +29,103 @@ import requests
 
 def main():
 
-    """
-    Main function. Pieces everything together.
-    """
+    mailer = XKCDMailer()
+    mailer.run()
 
-    # Grabs comic and metadata.
-    comic_url, mouseover_text, comic_title, comic_number = comic_getter()
+    print "RAN THE SCRIPT"
 
-    # Saves comic image and mouseover text to current directory.
-    comic = requests.get(comic_url)
-    comic_id = "xkcd #" + comic_number + " - " + comic_title
-    comic_filename = comic_id + ".png"
-    if comic.status_code == 200:
-        Image.open(StringIO(comic.content)).save("comics/" + comic_filename)
-        with open("comics/" + comic_id + " - mouseover text.txt", "wb") as f:
-            f.write(mouseover_text)
-
-    # Sends email.
-    email_sender(comic_filename, mouseover_text, comic_title, comic_number, comic_id)
-
-
-def comic_getter():
+class XKCDMailer(object):
 
     """
-    Gets the URL of the comic, as well as metadata:
-
-    Yeah, I know I didn't worry about the HTTP status code not being 200, but the world won't explode if it's not.
-
-    :return comic_url:      The URL at which the image for the comic resides.
-    :return mouseover_text: The mouseover text for the comic image.
-    :return comic_title:    The title of the comic.
-    :return comic_number:   The number of the comic.
+    Wrapper class for xkcd-emailing metadata and functionality.
     """
 
-    # Pulls the source code into a DOM tree.
-    page_request = requests.get("http://www.xkcd.com")
-    soup = BS(page_request.text)
+    def __init__(self):
 
-    # Gets the comic image and title and the mouseover text from the div in which the comic lives.
-    img_div = soup.find_all(id="comic")[0].find("img")
-    comic_url = img_div["src"]
-    mouseover_text = img_div["title"]
-    comic_title = img_div["alt"]
+        """
+        Constructs a new instance, initially with just to and from designations.
+        """
 
-    # Extracts the comic number from the supplied permalink URL.
-    text_with_number = unicode(soup.find_all(text=re.compile("Permanent link to this comic:"))[0]).encode("utf-8").lstrip()
-    comic_number = re.findall("\d+", text_with_number)[0]
-
-    return comic_url, mouseover_text, comic_title, comic_number
+        self.send_from = "xkcddaemon@gmail.com"
+        self.send_to = ["gregakinman@gmail.com"]
+        self.dump_dir_path = "/Users/gregakinman/Google Drive/Work/Projects/scripts/xkcd_mailer/comics/"
 
 
-def email_sender(comic_filename, mouseover_text, comic_title, comic_number, comic_id):
+    def run(self):
 
-    """
-    Sends the email.
+        """
+        Main method. Pieces everything together.
 
-    :param comic_filename:  The name of the file in which the comic image resides.
-    :param mouseover_text:  The mouseover text associated with the comic.
-    :param comic_title:     The title of the comic.
-    :param comic_number:    The number of the comic.
-    :param comic_id:        A string that uniquely identifies this comic (combining the metadata).
-    """
+        :param self:    The current instance.
+        """
 
-    send_from = "xkcddaemon@gmail.com"
-    send_to = ["gregakinman@gmail.com"]
+        # Saves comic image and mouseover text to current directory.
+        self.__comic_getter()
 
-    # Constructs the email message, including text, the image attachment, and metadata.
-    msg = MIMEMultipart(
-        From=send_from,
-        To=", ".join(send_to),
-    )
-    msg["Subject"] = comic_id
-    msg.attach(MIMEText("Mouseover text: \"" + mouseover_text + "\""))
-    with open("comics/" + comic_filename, "rb") as f:
-        msg.attach(MIMEImage(f.read()))
-
-    # Opens a Gmail SMTP server and sends the email.
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(send_from, "TendeBeneAltaPete")
-    server.sendmail(send_from, send_to, msg.as_string())
-    server.quit()
+        # Sends email.
+        self.__email_sender()
 
 
-main()
+    def __comic_getter(self):
+
+        """
+        Internal helper method. Gets the URL of the comic, as well as metadata:
+
+        Yeah, I know I didn't worry about the HTTP status code not being 200, but the world won't explode if it's not.
+
+        :param self:    The current instance.
+        """
+
+        # Pulls the source code into a DOM tree.
+        page_request = requests.get("http://www.xkcd.com")
+        soup = BS(page_request.text)
+
+        # Gets the comic image and title and the mouseover text from the div in which the comic lives.
+        img_div = soup.find_all(id="comic")[0].find("img")
+        comic_url = img_div["src"]
+        self.mouseover_text = img_div["title"]
+        comic_title = img_div["alt"]
+
+        # Extracts the comic number from the supplied permalink URL.
+        text_with_number = (unicode(soup.find_all(text=re.compile("Permanent link to this comic:"))[0])
+            .encode("utf-8").lstrip())
+        comic_number = re.findall("\d+", text_with_number)[0]
+
+        # Dumps the comic image and mouseover text to files in a subdirectory.
+        comic = requests.get(comic_url)
+        self.comic_id = "xkcd #" + comic_number + " - " + comic_title
+        self.comic_filename = self.comic_id + ".jpg"
+        if comic.status_code == 200:
+            Image.open(StringIO(comic.content)).save(self.dump_dir_path + self.comic_filename)
+            with open(self.dump_dir_path + self.comic_id + " - mouseover text.txt", "wb") as f:
+                f.write(self.mouseover_text)
+
+
+    def __email_sender(self):
+
+        """
+        Internal helper method. Sends the email.
+
+        :param self:    The current instance.
+        """
+
+        # Constructs the email message, including text, the image attachment, and metadata.
+        msg = MIMEMultipart(
+            From=self.send_from,
+            To=", ".join(self.send_to),
+        )
+        msg["Subject"] = self.comic_id
+        msg.attach(MIMEText("Mouseover text: \"" + self.mouseover_text + "\""))
+        with open(self.dump_dir_path + self.comic_filename, "rb") as f:
+            msg.attach(MIMEImage(f.read()))
+
+        # Opens a Gmail SMTP server and sends the email.
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(self.send_from, "TendeBeneAltaPete")
+        server.sendmail(self.send_from, self.send_to, msg.as_string())
+        server.quit()
+
+
+if __name__ == "__main__":
+    main()
